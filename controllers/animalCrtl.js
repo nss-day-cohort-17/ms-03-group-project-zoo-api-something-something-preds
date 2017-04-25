00115c37981f79b3070b5ad2e7a1c4fd9d2e3063
Ben Conner <ben.conner40@gmail.com>
2017-04-25 15:36:28 -0500

    animal crtl

---
+++ controllers/animalCrtl.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { bookshelf } = require('../db/database');
+const Animal = require('../models/animals')
+
+module.exports.getAnimals = (req, res, next) => {
+  Animal.getAll()
+  .then( (animals) => {
+    res.status(200).json(animals);
+  })
+  .catch( (error) => {
+    next(error);
+  });
+}
+module.exports.addAnimal = ({body}, res, next) => {
+  Animal.forge(body)
+  .save()
+  .then( () => res.status(201).json({"msg": "Nice POST, brah"}))
+  .catch( (error) => {
+    next(err);
+  });
+};
+module.exports.deleteAnimal= ({params: {id}}, res, next) => {
+  Animal.forge({id})
+  .destroy()
+  .then( (animal) => {
+    res.status(202).json(animal);
+  })
+  .catch( (err) => {
+    next(err);
+  });
+};
+module.exports.getZoonimal = ({query: {animalId}}, res, next) => {
+  console.log("Getting animalId", animalId)
+  Animal.forge({id: animalId})
+  .fetch({withRelated: ['zookeepers'], require: true})
+  .then( (anizoo) => {
+    res.status(200).json(anizoo)
+  })
+  .catch( (err) => {
+     next (err)
+  })
+}
